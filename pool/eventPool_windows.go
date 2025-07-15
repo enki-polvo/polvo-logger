0b037de7c411c6b1096d70a7b4e850f0dddd39b1
SungHyun Hong <69339846+hongsam14@users.noreply.github.com>
2025-07-15 10:41:16 +0900

    feat: develop eventPool for windows

---
+++ pool/eventPool_windows.go
@@ -0,0 +1,201 @@
+//go:build windows
+// +build windows
+
+// pool/eventPool.go
+
+package eventPool
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	model "github.com/enki-polvo/polvo-logger/model"
+	eventModel "github.com/enki-polvo/polvo-logger/model/event"
+)
+
+const (
+	ErrEventNotFound              = "event '%s' not found in pool"
+	ErrGetEventFromPoolFailed     = "failed to get event from pool"
+	ErrInvalidTypeAssertionInPool = "invalid type assertion for event pool"
+)
+
+var (
+	modelMapper = map[model.EventCode]func() any{
+		model.PROCESS_CREATION: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.PROCESS_CREATION
+			obj.CommonHeader.EventName = model.PROCESS_CREATION.String()
+			obj.Metadata = &eventModel.ProcessCreationMetadata{}
+			return obj
+		},
+		model.NETWORK_CONNECTION: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.NETWORK_CONNECTION
+			obj.CommonHeader.EventName = model.NETWORK_CONNECTION.String()
+			obj.Metadata = &eventModel.NetworkConnectionMetadata{}
+			return obj
+		},
+		model.DRIVER_LOAD: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.DRIVER_LOAD
+			obj.CommonHeader.EventName = model.DRIVER_LOAD.String()
+			obj.Metadata = &eventModel.DriverLoadMetadata{}
+			return obj
+		},
+		model.IMAGE_LOAD: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.IMAGE_LOAD
+			obj.CommonHeader.EventName = model.IMAGE_LOAD.String()
+			obj.Metadata = &eventModel.ImageLoadMetadata{}
+			return obj
+		},
+		model.PROCESS_ACCESS: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.PROCESS_ACCESS
+			obj.CommonHeader.EventName = model.PROCESS_ACCESS.String()
+			obj.Metadata = &eventModel.ProcessAccessMetadata{}
+			return obj
+		},
+		model.FILE_EVENT: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.FILE_EVENT
+			obj.CommonHeader.EventName = model.FILE_EVENT.String()
+			obj.Metadata = &eventModel.FileEventMetadata{}
+			return obj
+		},
+		model.REGISTRY_ADD: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.REGISTRY_ADD
+			obj.CommonHeader.EventName = model.REGISTRY_ADD.String()
+			obj.Metadata = &eventModel.RegistryAddMetadata{}
+			return obj
+		},
+		model.REGISTRY_DELETE: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.REGISTRY_DELETE
+			obj.CommonHeader.EventName = model.REGISTRY_DELETE.String()
+			obj.Metadata = &eventModel.RegistryDeleteMetadata{}
+			return obj
+		},
+		model.REGISTRY_SET: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.REGISTRY_SET
+			obj.CommonHeader.EventName = model.REGISTRY_SET.String()
+			obj.Metadata = &eventModel.RegistrySetMetadata{}
+			return obj
+		},
+		model.REGISTRY_EVENT: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.REGISTRY_EVENT
+			obj.CommonHeader.EventName = model.REGISTRY_EVENT.String()
+			obj.Metadata = &eventModel.RegistryEventMetadata{}
+			return obj
+		},
+		model.CREATE_STREAM_HASH: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.CREATE_STREAM_HASH
+			obj.CommonHeader.EventName = model.CREATE_STREAM_HASH.String()
+			obj.Metadata = &eventModel.CreateStreamHashMetadata{}
+			return obj
+		},
+		model.DNS_QUERY: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.DNS_QUERY
+			obj.CommonHeader.EventName = model.DNS_QUERY.String()
+			obj.Metadata = &eventModel.DnsQueryMetadata{}
+			return obj
+		},
+		model.FILE_DELETE: func() any {
+			obj := &model.CommonModel{}
+			obj.CommonHeader.EventCode = model.FILE_DELETE
+			obj.CommonHeader.EventName = model.FILE_DELETE.String()
+			obj.Metadata = &eventModel.FileDeleteMetadata{}
+			return obj
+		},
+	}
+)
+
+// Pool interface defines the methods for the object pool.
+type Pool interface {
+	Allocate(eventName model.EventCode) (*model.CommonModel, error)
+	Free(event eventModel.Event) error
+}
+
+// eventPool implements the Pool interface.
+type eventPool struct {
+	eventPoolMap sync.Map // key: eventModel.EventCode, value: *sync.Pool{eventModel.Event}
+}
+
+// newEventPool initializes a new event pool.
+func NewEventPool() Pool {
+	newPool := new(eventPool)
+
+	newPool.eventPoolMap = sync.Map{}
+	// create a pool for each event type
+	for eventCode, newFunc := range modelMapper {
+		newPool.eventPoolMap.Store(eventCode, &sync.Pool{
+			New: newFunc,
+		})
+	}
+	// create
+	return newPool
+}
+
+// Allocate retrieves an event model from the pool.
+func (op *eventPool) Allocate(eventName model.EventCode) (*model.CommonModel, error) {
+	var (
+		value     any
+		eventPool *sync.Pool
+		isExists  bool
+	)
+
+	// check if event pool exists
+	value, isExists = op.eventPoolMap.Load(eventName)
+	if !isExists {
+		return nil, fmt.Errorf(ErrEventNotFound, eventName.String())
+	}
+
+	// get event from pool
+	eventPool, ok := value.(*sync.Pool)
+	if !ok {
+		return nil, errors.New(ErrInvalidTypeAssertionInPool)
+	}
+
+	value = eventPool.Get()
+	if value == nil {
+		return nil, errors.New(ErrGetEventFromPoolFailed)
+	}
+
+	event, ok := value.(*model.CommonModel)
+	if !ok {
+		return nil, errors.New(ErrInvalidTypeAssertionInPool)
+	}
+	return event, nil
+}
+
+// Free puts an event model back into the pool.
+func (op *eventPool) Free(event eventModel.Event) error {
+	var (
+		value     any
+		eventPool *sync.Pool
+		isExists  bool
+		eventName model.EventCode
+	)
+
+	// get event name from event
+	eventName = event.(*model.CommonModel).EventCode
+	// check if event pool exists
+	value, isExists = op.eventPoolMap.Load(eventName)
+	if !isExists {
+		return fmt.Errorf(ErrEventNotFound, eventName.String())
+	}
+
+	// put event to pool
+	eventPool, ok := value.(*sync.Pool)
+	if !ok {
+		return errors.New(ErrInvalidTypeAssertionInPool)
+	}
+	eventPool.Put(event)
+	return nil
+}
---
+++ pool/eventPool_windows_test.go
@@ -0,0 +1,240 @@
+//go:build windows
+// +build windows
+
+package eventPool_test
+
+import (
+	"sync"
+	"testing"
+
+	"fmt"
+
+	model "github.com/enki-polvo/polvo-logger/model"
+	eventModel "github.com/enki-polvo/polvo-logger/model/event"
+	eventPool "github.com/enki-polvo/polvo-logger/pool"
+)
+
+// Test Initialization of the event pool
+func TestCreateNewPool(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	if pool == nil {
+		t.Fatal("Failed to create new event pool")
+	}
+}
+
+// Test Allocation of a valid event
+// Test that a valid event can be allocated from the pool
+func TestAllocateEvent(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	eventCode := model.PROCESS_CREATION
+
+	event, err := pool.Allocate(eventCode)
+	if err != nil {
+		t.Fatalf("Failed to allocate event: %v", err)
+	}
+
+	if event == nil {
+		t.Fatal("Allocated event is nil")
+	}
+
+	if event.EventCode != eventCode {
+		t.Fatalf("Allocated event code does not match expected code: got %v, want %v", event.EventCode, eventCode)
+	}
+
+	metadata, ok := event.Metadata.(*eventModel.ProcessCreationMetadata)
+	if !ok {
+		t.Fatalf("Allocated event is not of type ProcessCreateEvent")
+	}
+
+	metadata.ProcessId = 1234
+	metadata.ParentProcessId = 5678
+	metadata.User = "root"
+	metadata.CommandLine = "bash rm -rf /tmp"
+	metadata.ParentCommandLine = "PATH=/usr/bin:/bin"
+	metadata.Image = "/usr/bin/bash"
+
+	t.Logf("Allocated event: %v\n, metadata: %v\n", event, event.Metadata)
+
+	// Free the event back to the pool
+	err = pool.Free(event)
+	if err != nil {
+		t.Fatalf("Failed to free event: %v", err)
+	}
+}
+
+// Test Allocation of an invalid event
+// Test that an invalid event code returns an error
+func TestAllocateInvalidEvent(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	invalidEventCode := model.EventCode(999) // Assuming 999 is not a valid event code
+
+	event, err := pool.Allocate(invalidEventCode)
+	if err == nil {
+		t.Fatal("Expected error when allocating invalid event, but got none")
+	}
+
+	if event != nil {
+		t.Fatal("Allocated event should be nil for invalid event code")
+	}
+}
+
+// Test Freeing of a valid event
+// Test that a valid event can be freed back to the pool
+func TestFreeEvent(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	eventCode := model.PROCESS_CREATION
+
+	event, err := pool.Allocate(eventCode)
+	if err != nil {
+		t.Fatalf("Failed to allocate event: %v", err)
+	}
+
+	if event == nil {
+		t.Fatal("Allocated event is nil")
+	}
+
+	err = pool.Free(event)
+	if err != nil {
+		t.Fatalf("Failed to free event: %v", err)
+	}
+}
+
+// (DEPRECATED) Test Freeing of an invalid event
+// // Test Freeing of an invalid event
+// // Test that freeing an invalid event code returns an error
+// func TestFreeInvalidEvent(t *testing.T) {
+// 	pool := eventPool.NewEventPool()
+// 	invalidEventCode := model.EventCode(999)  // Assuming 999 is not a valid event code
+// 	event := &eventModel.ProcessCreateEvent{} // Create a dummy event
+
+// 	err := pool.Free(event)
+// 	if err == nil {
+// 		t.Fatal("Expected error when freeing invalid event, but got none")
+// 	}
+// }
+
+// Test Stress Test for Allocating and Freeing Events
+// This test checks the performance of allocating and freeing events in a loop
+func TestStressTestAllocateFree(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	eventCode := model.PROCESS_CREATION
+
+	for i := 0; i < 1000; i++ {
+		event, err := pool.Allocate(eventCode)
+		if err != nil {
+			t.Fatalf("Failed to allocate event: %v", err)
+		}
+
+		if event == nil {
+			t.Fatal("Allocated event is nil")
+		}
+
+		err = pool.Free(event)
+		if err != nil {
+			t.Fatalf("Failed to free event: %v", err)
+		}
+	}
+}
+
+// Test Stress Test for Allocating Invalid Events
+// This test checks the performance of allocating invalid events in a loop
+// and ensures that it returns an error
+func TestStressTestAllocateInvalidEvent(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	invalidEventCode := model.EventCode(999) // Assuming 999 is not a valid event code
+
+	for i := 0; i < 1000; i++ {
+		event, err := pool.Allocate(invalidEventCode)
+		if err == nil {
+			t.Fatal("Expected error when allocating invalid event, but got none")
+		}
+
+		if event != nil {
+			t.Fatal("Allocated event should be nil for invalid event code")
+		}
+	}
+}
+
+// Test Stress Test for Freeing Invalid Events in Multiple Goroutines
+// This test checks the performance of freeing invalid events in multiple goroutines
+// and ensures that it returns an error
+func TestStressTestMultipleGoroutines(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	eventCode := model.PROCESS_CREATION
+
+	var wg sync.WaitGroup
+	var errChan chan error
+
+	errChan = make(chan error, 10)
+	defer close(errChan)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100000; j++ {
+				event, err := pool.Allocate(eventCode)
+				if err != nil {
+					errChan <- fmt.Errorf("failed to allocate event: %v", err)
+					return
+				}
+
+				if event == nil {
+					errChan <- fmt.Errorf("allocated event is nil")
+					return
+				}
+
+				err = pool.Free(event)
+				if err != nil {
+					errChan <- fmt.Errorf("failed to free event: %v", err)
+					return
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	if len(errChan) > 0 {
+		for err := range errChan {
+			t.Log(err)
+		}
+		t.Fatal("Errors occurred during stress test")
+	}
+}
+
+// Test Stress Test for Invalid Events in Multiple Goroutines
+// This test checks the performance of allocating and freeing invalid events
+// in multiple goroutines and ensures that it returns an error
+func TestStressTestInvalidInMultipleGoroutines(t *testing.T) {
+	pool := eventPool.NewEventPool()
+	invalidEventCode := model.EventCode(999) // Assuming 999 is not a valid event code
+
+	var wg sync.WaitGroup
+	var errChan chan error
+	errChan = make(chan error, 10)
+	defer close(errChan)
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				event, err := pool.Allocate(invalidEventCode)
+				if err == nil {
+					errChan <- fmt.Errorf("expected error when allocating invalid event, but got none")
+					return
+				}
+				if event != nil {
+					errChan <- fmt.Errorf("allocated event should be nil for invalid event code")
+					return
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	if len(errChan) > 0 {
+		for err := range errChan {
+			t.Log(err)
+		}
+		t.Fatal("Errors occurred during stress test")
+	}
+}
