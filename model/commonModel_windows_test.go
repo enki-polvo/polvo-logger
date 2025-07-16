6372bcb562061bf5242dfde3923a2acdeea7dc7e
SungHyun Hong <69339846+hongsam14@users.noreply.github.com>
2025-07-16 17:31:51 +0900

    feat(commonModel): reflect windows log to test file

--- model/commonModel_windows_test.go
+++ model/commonModel_windows_test.go
@@ -16,7 +16,7 @@ func TestDecodeMetadata(t *testing.T) {
 	// This is a placeholder for actual test implementation
 	// You would typically use a library like "encoding/json" or "github.com/mitchellh/mapstructure"
 	// to decode the metadata into the appropriate struct.
-	data := `{"EventName":"ProcessCreate","Source":"eBPF","Timestamp":"2025-06-09T16:54:26.270720921+09:00","Metadata":{"PID":97469,"PPID":97467,"UID":1000,"Username":"shhong","TGID":97469,"Commandline":"sh sed s/-//","ENV":"GJS_DEBUG_TOPICS=JS ERROR;JS LOG,XDG_ACTIVATION_TOKEN=gnome-shell/System Monitor/3726-8-shhong-server_TIME15324537,LANGUAGE=en,LC_TIME=ko_KR.UTF-8,USER=shhong,XDG_SESSION_TYPE=x11,SHLVL=0,CLUTTER_DISABLE_MIPMAPPED_TEXT=1,HOME=/home/shhong,DESKTOP_SESSION=ubuntu","Image":"/usr/bin/sed"}}`
+	data := `{"EventID":0,"EventName":"process_creation","Source":"sysmon","TimeStamp":"2025-07-16T17:25:26.6982545+09:00","Metadata":{"CommandLine":"\"C:\\Program Files\\WindowsApps\\Microsoft.WindowsTerminal_1.22.11141.0_x64__8wekyb3d8bbwe\\WindowsTerminal.exe\" ","Company":"-","CurrentDirectory":"C:\\WINDOWS\\System32\\","FileVersion":"-","Hashes":"SHA1=3FE7F5EEBFA945B993760235BA4A1CD9DEDD5319,MD5=08480D610277845A42200424044B910F,SHA256=9FB7D27FF43617EC9BADAD2FDABFEB1DD76C2EBA31D990B81B6CEF4DC8CCA4B8,IMPHASH=CE008010C3B058594523090A70F63DF4","Image":"C:\\Program Files\\WindowsApps\\Microsoft.WindowsTerminal_1.22.11141.0_x64__8wekyb3d8bbwe\\WindowsTerminal.exe","IntegrityLevel":"Medium","LogonId":"140592926","OriginalFileName":"-","ParentCommandLine":"C:\\WINDOWS\\Explorer.EXE","ParentImage":"C:\\Windows\\explorer.exe","ParentProcessId":24044,"ProcessId":21964,"Product":"-","User":"DESKTOP-8S5JU8M\\user01"}}`
 
 	// decode data into CommonModelWrapper
 	cmw := &commonModel.CommonModelWrapper{}
@@ -44,7 +44,7 @@ func TestDecodeMetadata(t *testing.T) {
 
 func TestDecodeMetadataWithInvalidData(t *testing.T) {
 	// Example of how to test the decoding of metadata with an invalid type
-	data := `{"EventName":"ProcessCreate","Source":"eBPF","Timestamp":"2025-06-09T16:54:26.270720921+09:00","Metadata":{"PID":"invalid_type"}}`
+	data := `{"EventName":"ProcessCreate","Source":"eBPF","Timestamp":"2025-06-09T16:54:26.270720921+09:00","Metadata":{"ProcessId":"invalid_type"}}`
 
 	// decode data into CommonModelWrapper
 	cmw := &commonModel.CommonModelWrapper{}
@@ -54,15 +54,15 @@ func TestDecodeMetadataWithInvalidData(t *testing.T) {
 	}
 
 	// Attempt to decode into a specific type that expects a different structure
-	dest := &eventModel.ProcessCreationEvent{
+	dest := &eventModel.ProcessTerminatedEvent{
 		CommonHeader: commonModel.CommonHeader{
 			EventName: cmw.EventName,
 			Source:    cmw.Source,
 			Timestamp: cmw.Timestamp,
 		},
-		Metadata: eventModel.ProcessCreationMetadata{},
+		Metadata: eventModel.ProcessTerminatedMetadata{},
 	}
-	err = eventModel.DecodeMetadataAs[eventModel.ProcessCreationMetadata](cmw.Metadata, &dest.Metadata)
+	err = eventModel.DecodeMetadataAs[eventModel.ProcessTerminatedMetadata](cmw.Metadata, &dest.Metadata)
 	if err == nil {
 		t.Fatal("Expected an error due to invalid type, but got none")
 	}
@@ -71,8 +71,8 @@ func TestDecodeMetadataWithInvalidData(t *testing.T) {
 
 func TestDecodeMetadataWithDifferentDataType(t *testing.T) {
 	// Example of how to test the decoding of metadata with a different data type
-	data := `{"EventName":"ProcessCreate","Source":"eBPF","Timestamp":"2025-06-09T16:54:26.270720921+09:00","Metadata":{"PID":97469,"PPID":97467,"UID":1000,"Username":"shhong","TGID":97469,"Commandline":"sh sed s/-//","ENV":"GJS_DEBUG_TOPICS=JS ERROR;JS LOG,XDG_ACTIVATION_TOKEN=gnome-shell/System Monitor/3726-8-shhong-server_TIME15324537,LANGUAGE=en,LC_TIME=ko_KR.UTF-8,USER=shhong,XDG_SESSION_TYPE=x11,SHLVL=0,CLUTTER_DISABLE_MIPMAPPED_TEXT=1,HOME=/home/shhong,DESKTOP_SESSION=ubuntu","Image":"/usr/bin/sed"}}`
-
+	data := `{"EventID":0,"EventName":"process_creation","Source":"sysmon","TimeStamp":"2025-07-16T17:25:26.6982545+09:00","Metadata":{"CommandLine":"\"C:\\Program Files\\WindowsApps\\Microsoft.WindowsTerminal_1.22.11141.0_x64__8wekyb3d8bbwe\\WindowsTerminal.exe\" ","Company":"-","CurrentDirectory":"C:\\WINDOWS\\System32\\","FileVersion":"-","Hashes":"SHA1=3FE7F5EEBFA945B993760235BA4A1CD9DEDD5319,MD5=08480D610277845A42200424044B910F,SHA256=9FB7D27FF43617EC9BADAD2FDABFEB1DD76C2EBA31D990B81B6CEF4DC8CCA4B8,IMPHASH=CE008010C3B058594523090A70F63DF4","Image":"C:\\Program Files\\WindowsApps\\Microsoft.WindowsTerminal_1.22.11141.0_x64__8wekyb3d8bbwe\\WindowsTerminal.exe","IntegrityLevel":"Medium","LogonId":"140592926","OriginalFileName":"-","ParentCommandLine":"C:\\WINDOWS\\Explorer.EXE","ParentImage":"C:\\Windows\\explorer.exe","ParentProcessId":24044,"ProcessId":21964,"Product":"-","User":"DESKTOP-8S5JU8M\\user01"}}`
+	
 	// decode data into CommonModelWrapper
 	cmw := &commonModel.CommonModelWrapper{}
 	err := json.Unmarshal([]byte(data), cmw)
